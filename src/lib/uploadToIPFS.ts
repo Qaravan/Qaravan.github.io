a45b57a14d7854512d53b7bdc2d94c5842342714
Alex Baker <alex.baker.fon@gmail.com>
2022-11-18 15:51:42 +0200

    add: token

--- src/lib/uploadToIPFS.ts
+++ src/lib/uploadToIPFS.ts
@@ -6,7 +6,7 @@ export default function uploadToIPFS(file: any, cid: any) {
   axios
     .post("https://api.web3.storage/upload", formData, {
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGZFNjNDOWUxMDFCZjRDYTgzNWFhQjdkQTg0OTAwOTc3MTMwMjkxY0YiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Njc4NTc2NDczODksIm5hbWUiOiJBUEkifQ.dRV0-fmzbAHfASDC5YxCM6Ui_oRdP7r2kWIskUeC_48`,
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGNBYjNkQjI4ODY3MTAyMGU1NmYxNjU1MDVlQ0E0MGE4NDY2NGQwNWIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Njg3Nzk0MjE3MTcsIm5hbWUiOiJORlQifQ.4_2s-LFu0Qhp8DP8atx3-8I3r5CICAQ_byszxQzQ8nw`,
         "Content-Type": "multipart/form-data",
       },
     })
